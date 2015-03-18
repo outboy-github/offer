31672b3f9806bc4d98d0754768576735501ff49a
Yuanwen Wang <1010670018@qq.com>
2015-03-18 11:35:34 +0800

    Update offer_08_MinNumberInRotatedArray.java

--- offer_08_MinNumberInRotatedArray.java
+++ offer_08_MinNumberInRotatedArray.java
@@ -1,42 +1,37 @@
 package com.wyw.offer;
 
 public class Offer_08_MinNumberInRotatedArray {
-	
+
 	public static void main(String[] args){
 		
-		int[] array = {1,1,0,1,1,1};
-		int minNumIndex = getMinNumberInRotatedArray(array);
-		
-		System.out.println(array[minNumIndex]);
+//		int[] array = {1,0,1,1,1};
+		int[] array = {3,4,5,1,2};
+		int minNum = getMinNumberInRotatedArray(array);
+		System.out.println(minNum);
 	}
 	static int getMinNumberInRotatedArray(int[] array){
-		int minIndex = 0;
-		int index1 = 0;
-		int index2 = array.length - 1;
-		int mid = 0;//(index1 + index2) / 2;
+		int start = 0;
+		int end = array.length - 1;
+		int mid = (start + end) / 2;
 		
-		while(array[index1] >= array[index2]){
-			if(index1 + 1 == index2){
-				minIndex = index2;
-				break;
-			}
-			mid = (index1 + index2) / 2;
-			
-			if(array[first] == array[mid] && array[mid] == array[last]){
+		while(end - start != 1){
+			mid = (start + end) / 2;
+			if(array[start] == array[mid] && array[mid] == array[end]){
 				int tmp = 0;
 				for(int i = 1; i< array.length; i++){
 					if(array[tmp] > array[i]){
 						tmp = i;
 					}
 				}
-				return tmp;
+				return array[tmp];
 			}
-			if(array[mid] > array[index1]){
-				index1 = mid;
+			if(array[mid] > array[start]){
+				start = mid;
 			}
-			else if(array[mid] < array[index2]){
-				index2 = mid;
+			else if(array[mid] < array[end]){
+				end = mid;
 			}
 		}
-		return minIndex;
+		return array[end];
+	}
 }
