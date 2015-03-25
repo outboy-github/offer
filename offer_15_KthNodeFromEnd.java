b5d3f7cf1264aff628e9467794cf9ca8e019c731
Yuanwen Wang <1010670018@qq.com>
2015-03-25 21:56:53 +0800

    Create offer_15_KthNodeFromEnd.java

---
+++ offer_15_KthNodeFromEnd.java
@@ -0,0 +1,71 @@
+package com.wyw.offer;
+
+public class Offer_15_KthNodeFromEnd{
+
+	public static void main(String[] args){
+		
+		String[] array = {"a","b","c","d","e"};
+		int k = 7;
+		Node<String> first = parseArrayToLink(array);
+		
+		Node<String> kthNode = KthNodeFromEnd(first, k);
+		if(kthNode != null){
+			System.out.println(kthNode.value);
+		}
+		else{
+			System.out.println("array in null or k is out of the length of the array.");
+		}
+	}
+	static Node<String> KthNodeFromEnd(Node<String> first, int k){
+		
+		Node<String> kthNode = null;
+		
+		if(first == null || k == 0){
+			return null;
+		}
+		int start = 1;
+		int end = 1;
+		Node<String> node = first;
+		while(node != null){
+			node = node.next;
+			end++;
+			if(end == k){
+				break;
+			}
+		}
+		if(end < k){
+			return null;
+		}
+		kthNode = first;
+		while(node.next != null){
+			node = node.next;
+			kthNode = kthNode.next;
+		}
+		return kthNode;
+	}
+	
+	static Node<String> parseArrayToLink(String[] array){
+		
+		Node<String> first = null;
+		Node<String> tmp = null;
+		for(String str : array){
+			Node<String> node = new Node<String>(str);
+			if(first == null){
+				first = node;
+			}
+			else{
+				tmp.next = node;
+			}
+			tmp = node;
+		}
+		return first;
+	}
+	
+	static class Node<String>{
+		String value;
+		Node<String> next;
+		Node(String value){
+			this.value = value;
+		}
+	}
+}
