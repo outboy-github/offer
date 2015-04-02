0190aa003504c769e73204bc86581d594b59519c
Yuanwen Wang <1010670018@qq.com>
2015-04-02 17:17:57 +0800

    Update offer_36_InversePairs.java

--- offer_36_InversePairs.java
+++ offer_36_InversePairs.java
@@ -4,14 +4,18 @@ public class Offer_36_InversePairs {
 
 	public static void main(String[] args) {
 
-		int[] array = {7,5,6,8};
-		inversePairs(array);
+		int[] array = {7,5,6,4};
+		int result = inversePairs(array);
+		System.out.println(result);
 	}
-	static void inversePairs(int[] array){
+	static int inversePairs(int[] array){
 		int length = array.length;
 		int[] copy = new int[length];
+		for(int i = 0; i < length; i++){
+			copy[i] = array[i];
+		}
 		int result = inverse(array, copy, 0, length - 1);
-		System.out.println(result);
+		return result;
 	}
 	
 	static int inverse(int[] array, int[] copy, int start, int end){
@@ -30,6 +34,7 @@ public class Offer_36_InversePairs {
 		int j = end;
 		int nextIndex = end;
 		int count = 0;
+		
 		while(i >= start && j >= start + length +1){
 			if(array[i] > array[j]){
 				copy[nextIndex--] = array[i--]; 
@@ -46,6 +51,7 @@ public class Offer_36_InversePairs {
 		while(j >= start + length + 1){
 			copy[nextIndex--] = array[j--];
 		}
+		
 		return count + left + right;
 	}
 }
