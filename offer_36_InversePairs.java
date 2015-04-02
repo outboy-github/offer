111b4dccbe7f1af8c297127d7a044d28234ec483
Yuanwen Wang <1010670018@qq.com>
2015-04-02 14:35:14 +0800

    Create offer_36_InversePairs.java

---
+++ offer_36_InversePairs.java
@@ -0,0 +1,44 @@
+package com.wyw.offer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Offer_36_InversePairs {
+
+	public static void main(String[] args) {
+
+//		String string = "abaccdebff";
+		String string = "aabb";
+		FirstNotRepeatingChar(string);
+	}
+	static void FirstNotRepeatingChar(String string){
+		char firstChar = ' ';
+		if(string == null || string.trim().length() < 1){
+			System.out.println("invalid input.");
+			return;
+		}
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for(char ch : string.toCharArray()){
+			
+			if(!map.containsKey(ch)){
+				map.put(ch, 1);
+			}
+			else{
+				map.put(ch, Integer.valueOf(map.get(ch)) + 1);
+			}
+		}
+		for(Character ch : string.toCharArray()){
+			if(Integer.valueOf(map.get(ch)) == 1){
+				firstChar = ch;
+				break;
+			}
+		}
+		if(firstChar == ' '){
+			System.out.println("the not repeat char is not exist.");
+		}
+		else{
+			System.out.println(firstChar);
+		}
+	}
+}
