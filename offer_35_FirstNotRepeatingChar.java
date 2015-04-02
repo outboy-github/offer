c0bf47b124c0686808e55f7a08e2ee3f45e560b1
Yuanwen Wang <1010670018@qq.com>
2015-04-02 10:46:47 +0800

    Create offer_35_FirstNotRepeatingChar.java

---
+++ offer_35_FirstNotRepeatingChar.java
@@ -0,0 +1,48 @@
+package com.wyw.offer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Offer_35_FirstNotRepeatingChar {
+
+	public static void main(String[] args) {
+
+//		String string = "abaccdebff";
+		String string = "aabb";
+		FirstNotRepeatingChar(string);
+	}
+	static void FirstNotRepeatingChar(String string){
+		char firstChar = ' ';
+		if(string == null || string.trim().length() < 1){
+			System.out.println("invalid input.");
+			return;
+		}
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		List<Character> list = new ArrayList<Character>();
+		for(char ch : string.toCharArray()){
+			
+			if(!map.containsKey(ch)){
+				map.put(ch, 1);
+			}
+			else{
+				map.put(ch, Integer.valueOf(map.get(ch)) + 1);
+			}
+			list.add(ch);
+		}
+		for(Character ch : list){
+			if(Integer.valueOf(map.get(ch)) == 1){
+				firstChar = ch;
+				break;
+			}
+		}
+		if(firstChar == ' '){
+			System.out.println("the not repeat char is not exist.");
+		}
+		else{
+			System.out.println(firstChar);
+		}
+	}
+}
