d20661cb59a479f434b49107dfdcd08a13ee08e2
Yuanwen Wang <1010670018@qq.com>
2015-04-06 19:18:48 +0800

    Create offer_42_2_ContinuesSquenceWithSum.java

---
+++ offer_42_2_ContinuesSquenceWithSum.java
@@ -0,0 +1,40 @@
+package com.wyw.offer;
+
+public class Offer_42_2_ContinuesSquenceWithSum {
+
+	public static void main(String[] args) {
+		int sum = 15;
+		continuesSquenceWithSum(sum);
+	}
+	
+	static void continuesSquenceWithSum(int sum){
+		if(sum < 1){
+			System.out.println("the input is faulty");
+			return;
+		}
+		int small = 1;
+		int big = 2;
+		int curSum = small + big;
+		int middle = (1 + sum) / 2;
+		while(small < middle){
+			if(curSum == sum){
+				print(small, big);
+				curSum += ++big;
+			}
+			else if(curSum < sum){
+				curSum += ++big;
+			}
+			else{
+				curSum -= small++;
+			}
+		}
+	}
+	
+	static void print(int small, int big){
+		while(small <= big){
+			System.out.print(small + " ");
+			small++;
+		}
+		System.out.println();
+	}
+}
