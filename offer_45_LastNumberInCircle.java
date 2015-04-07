5a9599ba9dd03c4a07323e58140882403c0f2cba
Yuanwen Wang <1010670018@qq.com>
2015-04-07 16:24:57 +0800

    Create offer_45_LastNumberInCircle.java

---
+++ offer_45_LastNumberInCircle.java
@@ -0,0 +1,67 @@
+package com.wyw.offer;
+
+public class Offer_45_LastNumberInCircle {
+
+	public static void main(String[] args) {
+
+		int n = 5000;
+		int m = 999;
+		Node<Integer> tail = parseArrayToLinkCircleList(n);
+		if(tail == null){
+			System.out.println("there is no numbers.");
+		}
+		else{
+			int num = LastNumberInCircle(tail, m);
+			System.out.println("the last number is " + num);
+		}
+	}
+	
+	static Node<Integer> parseArrayToLinkCircleList(int n){
+		
+		Node<Integer> head = null;
+		Node<Integer> tmp = null;
+		
+		for(int i = 0; i < n; i++){
+		
+			Node<Integer> node = new Node<Integer>(i);
+			if(head == null){
+				head = node;
+				head.next = head;
+				tmp = head;
+			}
+			else{
+				tmp.next = node;
+				node.next = head;
+				tmp = node;
+			}
+		}
+		return tmp;
+	}
+	
+	static int LastNumberInCircle(Node<Integer> tail, int m){
+		
+		Node<Integer> node = tail;
+		while(node != node.next){
+			int k = 1;
+			while(k < m){
+				node = node.next;
+				k++;
+			}
+			//循环后node正好是head的前一个节点
+			Node<Integer> tmp = node.next;
+			System.out.print(tmp.value + " ");
+			node.next = tmp.next;
+			tmp.next = null;
+		}
+		System.out.println();
+		return node.value;
+	}
+	
+	static class Node<Integer>{
+		int value;
+		Node<Integer> next;
+		Node(int value){
+			this.value = value;
+		}
+	}
+}
